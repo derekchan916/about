f0b478c76ac06b934dcedca3537c3f06867549ed
Derek Chan <derekchan916@gmail.com>
2016-08-07 00:07:39 -0400

    Create file called content.

--- app/index.jsx
+++ app/index.jsx
@@ -7,12 +7,14 @@ import React, {
 import ReactDOM from 'react-dom';
 
 import Header from './src/Main/Header';
+import Content from './src/Main/Content';
 
 class Index extends Component {
 	render () {
 		return (
 			<div className="Styleguide">
 				<Header />
+				<Content />
 			</div>
 		)
 	}
---
+++ app/src/Main/Content.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import './Content.scss';
+import React, {
+	Component
+} from 'react';
+
+class Content extends Component {
+	render () {
+		return (
+			<div>hi!</div>
+		)
+	}
+}
+
+export default Content;
